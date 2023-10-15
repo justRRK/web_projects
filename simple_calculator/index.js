d2954fd013f20d6094932c9440527b6b0a8ed4e0
justRRK <r.rahul2636@gmail.com>
2023-10-15 15:28:06 +0530

    Bug fixes

--- simple_calculator/index.js
+++ simple_calculator/index.js
@@ -2,6 +2,9 @@ let displayBox = document.getElementById("displayBox");
 let buttons = document.querySelectorAll("button");
 
 let output = "";
+let isOperator = false;
+let isAns = false;
+let isDecimal = false;
 
 buttons.forEach((element) => {
   element.addEventListener("click", (btn) => {
@@ -9,18 +12,46 @@ buttons.forEach((element) => {
       output = String(eval(output));
       if (output != typeof undefined) {
         displayBox.value = output;
+        isAns = true;
       } else {
         output = "";
       }
     } else if (btn.target.innerText == "AC") {
       output = "";
       displayBox.value = output;
+      isAns = false;
+      isOperator = false;
+      isDecimal = false;
     } else if (btn.target.innerText == "DEL") {
+      if (output[output.length - 1] == ".") {
+        isDecimal = false;
+      }
       output = output.substring(0, output.length - 1);
       displayBox.value = output;
-    } else {
+      isAns = false;
+      isOperator = false;
+    } else if (btn.target.innerText == ".") {
+      if (!isDecimal) {
+        isDecimal = true;
+        isOperator = false;
+        output += btn.target.innerText;
+        displayBox.value = output;
+        isAns = false;
+      }
+    } else if (btn.target.className != "operator") {
+      if (isAns) {
+        output = "";
+      }
+      isOperator = false;
       output += btn.target.innerText;
       displayBox.value = output;
+      isAns = false;
+    } else if (!isOperator) {
+      isOperator = true;
+      output += btn.target.innerText;
+      displayBox.value = output;
+      isAns = false;
+      isDecimal = false;
     }
   });
 });
